ac8b26d4af3314a58a690c95ac730562bacc64e7
craftykate <katemcfaul@gmail.com>
2025-01-04 14:55:18 -0800

    First pass at metadata object

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,45 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "@/theme";
 import "@mantine/core/styles.css";
 import "./globals.css";
 
+export const viewport: Viewport = {
+  minimumScale: 1,
+  initialScale: 1,
+  maximumScale: 1,
+  width: "device-width",
+};
+
 export const metadata: Metadata = {
   title: "Easy MBTP Linker",
   description: "Quick links to MBTP resources",
+  appleWebApp: {
+    capable: true,
+    title: "Easy MBTP Linker",
+    statusBarStyle: "default",
+  },
+  openGraph: {
+    title: "Easy MBTP Linker",
+    description: "Quick links to MBTP resources",
+    url: "https://mbtp-linker.vercel.app",
+    type: "website",
+    images: [
+      {
+        url: "https://mbtp-linker.vercel.app/thumbnail.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    siteName: "mbtp-linker.vercel.app",
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Easy MBTP Linker",
+    description: "Quick links to MBTP resources",
+    images: "https://mbtp-linker.vercel.app/thumbnail.png",
+  },
 };
 
 export default function RootLayout({
@@ -18,10 +51,6 @@ export default function RootLayout({
     <html lang="en" data-mantine-color-scheme="light">
       {/* data-mantine-color-scheme tag added to suppress mantine hydration bug error */}
       <head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, maximum-scale=1"
-        />
         <ColorSchemeScript />
       </head>
       <body>
