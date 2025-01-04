70d2ccd653d022dda100e8a4fbd1aa8bf402a4b2
craftykate <katemcfaul@gmail.com>
2025-01-04 11:50:56 -0800

    Add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Container, Paper } from "@mantine/core";
+import Header from "@/components/layout/header/Header";
 import QuickLinks from "@/components/quick-links/QuickLinks";
 
 export default function Home() {
   return (
     <Container size="sm" mt="md">
       <Paper shadow="xs" mb="md" p="md" withBorder>
+        <Header />
         <QuickLinks />
       </Paper>
     </Container>
---
+++ components/layout/header/Header.tsx
@@ -0,0 +1,16 @@
+import { Text } from "@mantine/core";
+
+export default function Header() {
+  return (
+    <Text
+      component="h1"
+      size="3rem"
+      lh={1.1}
+      mb="sm"
+      variant="gradient"
+      gradient={{ from: "cyan", to: "purple", deg: 120 }}
+    >
+      Easy MBTP Linker
+    </Text>
+  );
+}
