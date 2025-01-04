1eaf30076cb9f0aa5c682990a4f1f891161e5c61
craftykate <katemcfaul@gmail.com>
2025-01-04 11:04:15 -0800

    Update styles with Mantine

---
+++ theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+  fontFamily: "Quicksand, Helvetica, Arial, sans-serif",
+  primaryColor: "cyan",
+  colors: {
+    "error-red": [
+      "#ffeaec",
+      "#fdd4d6",
+      "#f4a7ac",
+      "#ec777e",
+      "#e64f57",
+      "#e3353f",
+      "#e22732",
+      "#c91a25",
+      "#b31220",
+      "#9e0419",
+    ],
+  },
+});
