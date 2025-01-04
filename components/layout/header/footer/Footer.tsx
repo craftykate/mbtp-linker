cbf31d505f8371dad3293f4b6d268fc33bf985d1
craftykate <katemcfaul@gmail.com>
2025-01-04 11:52:21 -0800

    Add footer

---
+++ components/layout/header/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@mantine/core";
+
+export default function Footer() {
+  return (
+    <Box ta="center">
+      <Text size="sm">
+        &copy; {new Date().getFullYear()} Easy MBTP Linker. All rights reserved.
+        Site built by Kate McFaul.
+      </Text>
+      <Text size="sm">Not affiliated with Moving Beyond the Page.</Text>
+    </Box>
+  );
+}
