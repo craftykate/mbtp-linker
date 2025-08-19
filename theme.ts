825493bcb850b1852770580a41e76f106d9ce127
craftykate <katemcfaul@gmail.com>
2025-08-18 19:08:37 -0700

    Add cyan as a color

--- theme.ts
+++ theme.ts
@@ -4,6 +4,18 @@ export const theme = createTheme({
   fontFamily: "Quicksand, Helvetica, Arial, sans-serif",
   primaryColor: "cyan",
   colors: {
+    cyan: [
+      "#f0f9ff",
+      "#e0f2fe",
+      "#bae6fd",
+      "#7dd3fc",
+      "#38bdf8",
+      "#0ea5e9",
+      "#0284c7",
+      "#0369a1",
+      "#075985",
+      "#0c4a6e",
+    ],
     "error-red": [
       "#ffeaec",
       "#fdd4d6",
