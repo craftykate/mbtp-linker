9ce6e4f5d2ee4bfc65b48e61341debf02df7077b
craftykate <katemcfaul@gmail.com>
2025-11-07 15:10:06 -0800

    Updated shutdown message

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { Anchor, Button, Paper, Text } from "@mantine/core";
+import { Button, Paper, Text, Image } from "@mantine/core";
 import { IconExternalLink } from "@tabler/icons-react";
 import { logUi } from "@/lib/logging/client-logging/logClient";
+import HomeroomHub from "@/public/images/homeroom-hub.png";
+import BTP from "@/public/images/btp-screenshot.png";
 
 export default function Header() {
   const handleLogClick = (
@@ -35,26 +37,18 @@ export default function Header() {
       <Paper withBorder p="md">
         <Text c="red" size="lg">
           <Text span fw="bold">
-            We&apos;ve moved!
+            Come join us!
           </Text>
         </Text>
+        <Text mb="md">
+          We are <b>shutting this site down</b> at the end of the year, but
+          don&apos;t worry, Homeroom Hub has everything you see here and SO MUCH
+          MORE. Plus, with a FREE account you can sync your BTP links across
+          devices and store timer sessions.
+        </Text>
         <Text>
-          Visit the new{" "}
-          <Anchor
-            href="https://homeroom-hub.vercel.app"
-            onClick={() =>
-              handleLogClick(
-                "external_link_click",
-                "homeroom-hub",
-                "https://homeroom-hub.vercel.app",
-                "Homeroom Hub"
-              )
-            }
-          >
-            Homeroom Hub
-          </Anchor>{" "}
-          - all the same kid-safe tools, now better than ever. Sync links and
-          timers across devices, with more features coming soon!
+          Get math help, write stories, play with color, build spelling quizzes,
+          roll dice for math games, and more.
         </Text>
         <Button
           rightSection={<IconExternalLink size="16" />}
@@ -69,6 +63,39 @@ export default function Header() {
           }
         >
           Visit Homeroom Hub
+        </Button>
+        <Text mt="xl">I mean, just look at all these tools:</Text>
+        <Image
+          src={HomeroomHub.src}
+          alt="Homeroom Hub"
+          mt="md"
+          radius="md"
+          width="100%"
+        />
+
+        <Text mt="xl">Look how pretty the new BTP linker looks:</Text>
+        <Image
+          src={BTP.src}
+          alt="BTP Linker"
+          mt="md"
+          radius="md"
+          width="100%"
+        />
+
+        <Text mt="xl">New tools are getting added all the time!</Text>
+        <Button
+          rightSection={<IconExternalLink size="16" />}
+          mt="md"
+          onClick={() =>
+            handleLogClick(
+              "button_click",
+              "homeroom-hub",
+              "https://homeroom-hub.vercel.app",
+              "Homeroom Hub"
+            )
+          }
+        >
+          Let&apos;s go!
         </Button>
       </Paper>
     </>
